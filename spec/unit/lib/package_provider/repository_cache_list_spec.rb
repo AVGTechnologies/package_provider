522cf30246e036c83675899d4503c3ff32e81a94
Ondřej Hošák <ondrej.hosak@avg.com>
2015-09-10 12:14:59 +0200

    feat: RepositoryCacheList
    - add class maintaining information about locally cloned repositories for faster initilization

---
+++ spec/unit/lib/package_provider/repository_cache_list_spec.rb
@@ -0,0 +1,29 @@
+describe PackageProvider::RepositoryCacheList do
+  path = File.join(PackageProvider.root, 'config')
+  repo_url = 'git@github.com:AVGTechnologies/package_provider.git'
+
+  before(:all) do
+    FakeFS.activate!
+
+    FileUtils.mkdir_p(path)
+    File.open("#{path}/repository_cache.yml", 'w+') do |f|
+      f.puts YAML.dump(
+        test: { local_repo_paths: { repo_url => path } })
+    end
+  end
+
+  after(:all) do
+    FakeFS.deactivate!
+  end
+
+  let(:subject) { PackageProvider::PackagePacker.new(package_path) }
+
+  describe '::find' do
+    it 'returns config settings for git repo url' do
+      expect(PackageProvider::RepositoryCacheList.find(repo_url)).to eq(path)
+    end
+    it 'return nil for unknown repo url' do
+      expect(PackageProvider::RepositoryCacheList.find('repo_url')).to be nil
+    end
+  end
+end
