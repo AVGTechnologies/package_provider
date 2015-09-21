2d3a248a4a393e1515287d8c863493d155dbb749
Ondřej Hošák <ondrej.hosak@avg.com>
2015-09-21 13:54:05 +0200

    feat: RepositoryConnectionPool
    - add repository connection pool class for worker

---
+++ lib/package_provider/repository_connection_pool.rb
@@ -0,0 +1,24 @@
+require 'connection_pool'
+require 'package_provider/cached_repository'
+
+module PackageProvider
+  # Prepared repositories on application start
+  class RepositoryConnectionPool
+    def initialize
+      @repos = {}
+    end
+
+    def fetch(repo)
+      repo_config = PackageProvider::RepositoryConfig.find(repo)
+      @repos[repo] ||= ConnectionPool.new(
+        size: repo_config[:pool_size],
+        timeout: repo_config[:timeout]
+      ) do
+        PackageProvider::CachedRepository.new(
+          repo,
+          repo_config[:cache_dir]
+        )
+      end
+    end
+  end
+end
