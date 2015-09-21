74bc9d2fc1e41469a2e9c42d9ee131dac4ede4dd
Ondřej Hošák <ondrej.hosak@avg.com>
2015-09-21 16:06:51 +0200

    refactor: CachedRepository
    - added method for testing if repository clone is in progress
    - change log method call order
    
    CachedRepository

--- lib/package_provider/cached_repository.rb
+++ lib/package_provider/cached_repository.rb
@@ -14,6 +14,11 @@ module PackageProvider
         repo_ready?(dir)
       end
 
+      def in_progress?(treeish, paths, use_submodules = false)
+        dir = cache_dir(treeish, paths, use_submodules)
+        File.exist?("#{dir}.clone_lock")
+      end
+
       def cache_dir(treeish, paths, use_submodules)
         @sha256 ||= Digest::SHA256.new
         h = { treeish: treeish, paths: paths, submodule: use_submodules }
@@ -31,21 +36,21 @@ module PackageProvider
 
     def cached_clone(treeish, paths, use_submodules = false)
       cached_dir = CachedRepository.cache_dir(treeish, paths, use_submodules)
-      return cached_dir if CachedRepository.repo_ready?(cached_dir)
-
-      locked_file = nil
-      begin
-        locked_file = lock_repo(cached_dir)
-      rescue Timeout::Error
-        return cached_dir if CachedRepository.repo_ready?(cached_dir)
-        raise CloneInProgress
+      if CachedRepository.repo_ready?(cached_dir)
+        Metriks.meter('packageprovider.repository.cached').mark
+        Metriks.meter("packageprovider.repository.#{metriks_key}.cached").mark
+        return cached_dir
       end
 
+      locked_file = lock_repo(cached_dir)
+
       begin
-        log_clone(cached_dir, treeish, paths, use_submodules)
+        logger.info("Started clonning: #{treeish} #{paths.inspect} " \
+          "#{use_submodules} into #{cached_dir}")
         clone(cached_dir, treeish, paths, use_submodules)
       rescue => err
-        log_error(err, cached_dir, treeish, paths, use_submodules)
+        logger.error("Expeption when clonning: #{treeish} #{paths.inspect} " \
+          "#{use_submodules} into #{cached_dir} err: #{err}")
         FileUtils.rm_rf(cached_dir)
         raise
       end
@@ -53,46 +58,38 @@ module PackageProvider
       repo_ready!(cached_dir)
       cached_dir
     ensure
-      log_unlock_repo(treeish, paths, use_submodules)
       unlock_repo(locked_file)
     end
 
     private
+
+    def logger
+      PackageProvider.logger
+    end
 
     def lock_repo(path)
       Timeout.timeout(2) do
         lock_file = "#{path}.clone_lock"
         f = File.open(lock_file, File::RDWR | File::CREAT, 0644)
         f.flock(File::LOCK_EX)
+        logger.info("Locking file #{lock_file}")
         f
       end
+    rescue Timeout::Error
+      Metriks.meter('packageprovider.repository.locked').mark
+      Metriks.meter("packageprovider.repository.#{metriks_key}.locked").mark
+      raise CloneInProgress
     end
 
     def unlock_repo(f)
       return unless f
       f.flock(File::LOCK_UN)
+      logger.info("Unlocking file #{f.path}")
       File.delete(f.path)
     end
 
     def repo_ready!(path)
       FileUtils.touch("#{path}.package_part_ready")
-    end
-
-    def log_error(err, dir, treeish, paths, use_submodules)
-      PackageProvider.logger.error(
-        "Expeption when clonning: #{treeish} #{paths.inspect} " \
-        "#{use_submodules} into #{dir} err: #{err}")
-    end
-
-    def log_clone(dir, treeish, paths, use_submodules)
-      PackageProvider.logger.debug(
-        "Started clonning: #{treeish} #{paths.inspect} #{use_submodules} " \
-        "into #{dir}")
-    end
-
-    def log_unlock_repo(treeish, paths, use_submodules)
-      PackageProvider.logger.debug(
-        "Unlocked file for: #{treeish} #{paths.inspect} #{use_submodules}")
     end
   end
 end
--- spec/unit/lib/package_provider/repository_spec.rb
+++ spec/unit/lib/package_provider/repository_spec.rb
@@ -7,21 +7,50 @@ describe PackageProvider::Repository do
   PackageProvider::Repository.temp_prefix = "pp_tests_#{rand(1000)}"
 
   let(:repo) do
-    status = double(:status)
-    expect(status).to receive(:success?).twice.and_return(true)
-    expect(Open3).to(
-      receive(:capture3)
-      .with({ 'ENV' => PackageProvider.env }, /.*init_repo.sh/, any_args)
-      .once
-      .and_return(['', '', status])
-    )
-
     PackageProvider::Repository.new(
       'https://github.com/AVGTechnologies/package_provider')
   end
 
+  let(:repo2) do
+    PackageProvider::Repository.new(
+      'git@github.com:ondrej-hosak/package_provider.git')
+  end
+
+  let(:repo3) do
+    PackageProvider::Repository.new(
+      'https://ondrej.hosak@stash.megacorp.com/scm/xxx/package_provider.git')
+  end
+
+  let(:repo4) do
+    PackageProvider::Repository.new(
+      'ssh://git@stash.megacorp.com:8085/xxx/package_provider.git')
+  end
+
+  let(:repo5) do
+    PackageProvider::Repository.new(
+      '/tmp/package_provider')
+  end
+
+  let(:repo6) do
+    PackageProvider::Repository.new(
+      '/package_provider')
+  end
+
   before(:all) do
     FakeFS.activate!
+  end
+
+  before(:each) do |example|
+    unless example.metadata[:skip_hook]
+      status = double(:status)
+      expect(status).to receive(:success?).twice.and_return(true)
+      expect(Open3).to(
+        receive(:capture3)
+        .with({ 'ENV' => PackageProvider.env }, /.*init_repo.sh/, any_args)
+        .once
+        .and_return(['', '', status])
+      )
+    end
   end
 
   after(:all) do
@@ -33,7 +62,7 @@ describe PackageProvider::Repository do
       expect(Dir.exist?(repo.repo_root)).to be true
     end
 
-    it 'raise exception when local folder to clone does not exists' do
+    it 'raise exception if local folder to clone does\'nt exists', :skip_hook do
       expect do
         PackageProvider::Repository.new(
           'https://github.com/AVGTechnologies/package_provider',
@@ -99,6 +128,35 @@ describe PackageProvider::Repository do
       expect(Dir.exist?(repo.repo_root)).to be true
       repo.destroy
       expect(Dir.exist?(repo.repo_root)).to be false
+    end
+  end
+
+  describe '#metriks_key' do
+    context 'returns metricks key for' do
+      it 'http://github' do
+        expect(repo.send(:metriks_key))
+          .to eq('AVGTechnologies_package_provider')
+      end
+
+      it 'ssh://github' do
+        expect(repo2.send(:metriks_key)).to eq('ondrej_hosak_package_provider')
+      end
+
+      it 'https://stash' do
+        expect(repo3.send(:metriks_key)).to eq('scm_xxx_package_provider')
+      end
+
+      it 'ssh://stash' do
+        expect(repo4.send(:metriks_key)).to eq('xxx_package_provider')
+      end
+
+      it 'local path' do
+        expect(repo5.send(:metriks_key)).to eq('tmp_package_provider')
+      end
+
+      it 'local short path' do
+        expect(repo6.send(:metriks_key)).to eq('package_provider')
+      end
     end
   end
 end
