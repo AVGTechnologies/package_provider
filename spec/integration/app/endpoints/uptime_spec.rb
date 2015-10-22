07cfd086a8109e855bcd86d8424a640e8b784bfe
Ondřej Hošák <ondrej.hosak@avg.com>
2015-10-22 13:48:08 +0200

    feat: add additional data to uptime route

--- spec/integration/app/endpoints/uptime_spec.rb
+++ spec/integration/app/endpoints/uptime_spec.rb
@@ -19,13 +19,16 @@ describe 'Application API uptime' do
     it 'responds to uptime' do
       allow(Sidekiq::Queue).to receive_message_chain(:new, :size) { 0 }
       allow(Sidekiq::Queue).to receive_message_chain(:new, :latency) { 0 }
+      allow(Sidekiq::RetrySet).to receive_message_chain(:new, :size) { 0 }
+      allow(Sidekiq::ScheduledSet).to receive_message_chain(:new, :size) { 0 }
 
       response = get "#{prefix}/uptime", {}, headers_json
 
       expect(response.body).to eq(
         { uptime: PackageProvider.start_time,
           packer_queue: { size: 0, latency: 0 },
-          repository_queue: { size: 0, latency: 0 }
+          repository_queue: { size: 0, latency: 0 },
+          sidekiq_queues: { RetrySet: 0, ScheduledSet: 0 }
         }.to_json)
     end
   end
